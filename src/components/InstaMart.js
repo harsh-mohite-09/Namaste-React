ef508938bc214fb3baa977479cdb7dd00d7fff1a
harsh-mohite-09 <harsh.mohite009@gmail.com>
2023-02-23 16:23:06 +0530

    Update InstaMart.js
    Created a collapsible accordion component

--- src/components/InstaMart.js
+++ src/components/InstaMart.js
@@ -1,5 +1,54 @@
+import { useEffect, useState } from "react";
+
+const Section = ({ title, description, isVisible, setIsVisible, secKey }) => {
+  // const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="border border-black p-2 m-2">
+      <h3 className="font-bold text-xl inline-block mr-3">{title}</h3>
+      <button
+        className="border border-black rounded-md p-1 w-[60px]"
+        onClick={() => (isVisible ? setIsVisible("") : setIsVisible(secKey))}
+      >
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
+
 const InstaMart = () => {
-  return <h1>Welcome to InstaMart</h1>;
+  const [visibleSection, setVisibleSection] = useState("");
+
+  return (
+    <div>
+      <h1 className="text-3xl p-2 m-2 font-bold">InstaMart</h1>
+      <Section
+        isVisible={visibleSection === "about"}
+        setIsVisible={setVisibleSection}
+        title={"About Instamart"}
+        secKey="about"
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illum ipsam veritatis non rerum odit magnam dolor quo incidunt. Quia inventore distinctio repellendus laudantium vitae architecto provident possimus, laborum dolorem.
+                    Non, voluptate modi esse e culpa quae dignissimos? Pariatur, sit aliquam fugiat nulla amet error? Magni!"
+      />
+      <Section
+        isVisible={visibleSection === "team"}
+        setIsVisible={setVisibleSection}
+        secKey="team"
+        title={"Team Instamart"}
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illum ipsam veritatis non rerum odit magnam dolor quo incidunt. Quia inventore distinctio repellendus laudantium vitae architecto provident possimus, laborum dolorem.
+                    Non, voluptate modi esse e culpa quae dignissimos? Pariatur, sit aliquam fugiat nulla amet error? Magni!"
+      />
+      <Section
+        isVisible={visibleSection === "careers"}
+        setIsVisible={setVisibleSection}
+        secKey="careers"
+        title={"Careers"}
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illum ipsam veritatis non rerum odit magnam dolor quo incidunt. Quia inventore distinctio repellendus laudantium vitae architecto provident possimus, laborum dolorem.
+                    Non, voluptate modi esse e culpa quae dignissimos? Pariatur, sit aliquam fugiat nulla amet error? Magni!"
+      />
+    </div>
+  );
 };
 
 export default InstaMart;
